eb5196a289047dfca100e32e1ed674d6001c329d
Akhilrajan-V <Akhilrajan-V>
2022-10-13 20:38:01 -0400

    Package upload

---
+++ Config/model_config.py
@@ -0,0 +1,94 @@
+import warnings
+from typing import Dict, List
+
+import torch
+import torch.nn as nn
+from torchvision.models.resnet import resnet18, resnet50
+from torchvision.models import vit_b_16
+from torchvision.models.googlenet import googlenet
+
+
+class RasterizedPlanningModel(nn.Module):
+    """Raster-based model for planning.
+    """
+
+    def __init__(
+            self,
+            model_arch: str,
+            num_input_channels: int,
+            num_targets: int,
+            weights_scaling: List[float],
+            criterion: nn.Module,
+            pretrained: bool = False,
+    ) -> None:
+        super().__init__()
+        self.model_arch = model_arch
+        self.num_input_channels = num_input_channels
+        self.num_targets = num_targets
+        self.register_buffer("weights_scaling", torch.tensor(weights_scaling))
+        self.pretrained = pretrained
+        self.criterion = criterion
+
+        if pretrained and self.num_input_channels != 3:
+            warnings.warn("There is no pre-trained model with num_in_channels != 3, first layer will be reset")
+
+        if model_arch == "resnet18":
+            self.model = resnet18(pretrained=pretrained)
+            self.model.fc = nn.Linear(in_features=512, out_features=num_targets)
+        elif model_arch == "vit16":
+            self.model = vit_b_16(pretrained=pretrained)
+            self.fc = nn.Sequential(nn.Linear(in_features=1024, out_features=512),
+                                    nn.Linear(in_features=512, out_features=num_targets))
+        elif model_arch == "resnet50":
+            self.model = resnet50(pretrained=pretrained)
+            self.model.fc = nn.Linear(in_features=2048, out_features=num_targets)
+        elif model_arch == "googlenet":
+            self.model = googlenet(pretrained=pretrained)
+            self.model.fc = nn.Linear(in_features=2048, out_features=num_targets)
+        else:
+            raise NotImplementedError(f"Model arch {model_arch} unknown")
+
+        if self.num_input_channels != 3:
+            if self.model_arch == "vit16":
+                self.model.conv_proj = torch.nn.Conv2d(self.num_input_channels, 768, kernel_size=(16, 16),
+                                                       stride=(16, 16))
+            elif self.model_arch == "googlenet":
+                self.model.conv1 = nn.Conv2d(self.num_input_channels, self.model.conv1.conv.out_channels,
+                                             kernel_size=self.model.conv1.conv.kernel_size,
+                                             stride=self.model.conv1.conv.stride, padding=self.model.conv1.conv.padding,
+                                             bias=False, )
+            else:
+                self.model.conv1 = nn.Conv2d(
+                    self.num_input_channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False
+                )
+
+    def forward(self, data_batch: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        # [batch_size, channels, height, width]
+        image_batch = data_batch["image"]
+        # [batch_size, num_steps * 2]
+        outputs = self.model(image_batch)
+        batch_size = len(data_batch["image"])
+
+        if self.training:
+            if self.criterion is None:
+                raise NotImplementedError("Loss function is undefined.")
+
+            # [batch_size, num_steps * 2]
+            targets = (torch.cat((data_batch["target_positions"], data_batch["target_yaws"]), dim=2)).view(
+                batch_size, -1
+            )
+            # [batch_size, num_steps]
+            target_weights = (data_batch["target_availabilities"].unsqueeze(-1) * self.weights_scaling).view(
+                batch_size, -1
+            )
+            loss = torch.mean(self.criterion(outputs, targets) * target_weights)
+            train_dict = {"loss": loss}
+            return train_dict
+        else:
+            predicted = outputs.view(batch_size, -1, 3)
+            # [batch_size, num_steps, 2->(XY)]
+            pred_positions = predicted[:, :, :2]
+            # [batch_size, num_steps, 1->(yaw)]
+            pred_yaws = predicted[:, :, 2:3]
+            eval_dict = {"positions": pred_positions, "yaws": pred_yaws}
+            return eval_dict
